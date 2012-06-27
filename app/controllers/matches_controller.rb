31125ab99200ff6167a61187d5e33c43fe4716a0
Matt Steedman <matt@tankjetstudios.com>
2012-06-26 23:23:33 -0400

    took out some comments

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -91,16 +91,6 @@ class MatchesController < ApplicationController
     else
       render json: {error: "couldn't save"}
     end
-
-    # respond_to do |format|
-    #   if @user.update_attributes(params[:user])
-    #     format.html { redirect_to @user, notice: 'User was successfully updated.' }
-    #     format.json { head :no_content }
-    #   else
-    #     format.html { render action: "edit" }
-    #     format.json { render json: @user.errors, status: :unprocessable_entity }
-    #   end
-    # end
   end
 
   # DELETE /matches/1
@@ -139,6 +129,5 @@ class MatchesController < ApplicationController
       render json: {msg: 'not your turn'}
     end
   end
-
 
 end
