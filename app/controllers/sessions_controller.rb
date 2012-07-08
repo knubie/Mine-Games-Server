2e48f970e927fcb479f89ee4ec8928f900639a5c
Matt Steedman <matt@tankjetstudios.com>
2012-07-08 10:34:25 -0400

    changed signin action to use new facebook method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,20 +7,30 @@ class SessionsController < ApplicationController
 			else
 				@user = {error: 'invalid email/password'}
 			end
-      render json: @user
-		elsif request.env['omniauth.auth']
-	  	omniauth = request.env["omniauth.auth"]
-	  	# render :text => request.env["omniauth.auth"].to_yaml
-			@user = User.find_or_create_by_uid(omniauth[:uid])
-			Rails.logger.info(@user.errors.inspect) 
-			@user.username ||= "#{omniauth[:info][:first_name]} #{omniauth[:info][:last_name][0]}."
-			@user.email ||= omniauth[:info][:email]
+			render json: @user
+		elsif params.has_key? :facebook
+			facebook = params[:facebook]
+			@user = User.find_or_create_by_uid(facebook[:uid])
+			@user.username ||= "#{facebook[:name].split(' ')[0]} #{facebook[:name].split(' ')[1][0]}."
+			@user.email ||= "email@facebook.com"
 			@user.password ||= @user.password_confirmation ||= SecureRandom.base64 # Unused but needed to pass validation
-			@user.token = omniauth[:credentials][:token]
+			@user.token = facebook[:token]
 			@user.save
 			sign_in @user
-			redirect_to "/access_token/#{@user.token}"
-		end
+
+		# elsif request.env['omniauth.auth']
+		#   	omniauth = request.env["omniauth.auth"]
+		#   	# render :text => request.env["omniauth.auth"].to_yaml
+		# 	@user = User.find_or_create_by_uid(omniauth[:uid])
+		# 	Rails.logger.info(@user.errors.inspect) 
+		# 	@user.username ||= "#{omniauth[:info][:first_name]} #{omniauth[:info][:last_name][0]}."
+		# 	@user.email ||= omniauth[:info][:email]
+		# 	@user.password ||= @user.password_confirmation ||= SecureRandom.base64 # Unused but needed to pass validation
+		# 	@user.token = omniauth[:credentials][:token]
+		# 	@user.save
+		# 	sign_in @user
+		# 	redirect_to "/access_token/#{@user.token}"
+		# end
 	end
 
 	def destroy
